80c45d7f1982fedcc309a9d37e1e839fa10a5ee4
timochan <i@timochan.cn>
2023-07-17 21:21:57 +0800

    chore: update

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
  * @Author: timochan
  * @Date: 2023-07-17 11:48:02
  * @LastEditors: timochan
- * @LastEditTime: 2023-07-17 17:37:00
+ * @LastEditTime: 2023-07-17 21:21:00
  * @FilePath: /processforlinux/src/main.rs
  */
 mod get_active_window;
@@ -14,7 +14,8 @@ fn main() {
     loop {
         let rt = Runtime::new().unwrap();
         let (api_url, api_key, report_time) = get_env_file::init().unwrap();
-        let media_title = get_media::get_media_name();
+        //TODO: media not work
+        let media_title = "None";
         let process_name = get_active_window::get_active_window_process_and_title().unwrap();
 
         rt.block_on(report(&process_name, &media_title, &api_key, &api_url));
