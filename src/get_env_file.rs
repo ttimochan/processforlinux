209a8dc4b302043022301145ebb3cd13d4a6c8c6
timochan <i@timochan.cn>
2023-07-18 17:27:12 +0800

    fix: read file error

--- src/get_env_file.rs
+++ src/get_env_file.rs
@@ -2,7 +2,7 @@
  * @Author: timochan
  * @Date: 2023-07-17 13:51:34
  * @LastEditors: timochan
- * @LastEditTime: 2023-07-18 11:44:22
+ * @LastEditTime: 2023-07-18 17:26:05
  * @FilePath: /processforlinux/src/get_env_file.rs
  */
 use clap::{App, Arg};
@@ -20,18 +20,27 @@ fn read_config_values(config_path: &str) -> Option<(String, String, String, Stri
     let mut media_enable = None;
     let mut log_enable = None;
 
-    for line in reader.lines() {
-        let line = line.ok()?;
-        if line.starts_with("API_URL=") {
-            api_url = Some(line.trim_start_matches("API_URL=").to_string());
-        } else if line.starts_with("API_KEY=") {
-            api_key = Some(line.trim_start_matches("API_KEY=").to_string());
-        } else if line.starts_with("REPORT_TIME=") {
-            report_time = Some(line.trim_start_matches("REPORT_TIME=").to_string());
-        } else if line.starts_with("MEDIA_ENABLE=") {
-            media_enable = Some(line.trim_start_matches("MEDIA_ENABLE=").to_string());
-        } else if line.starts_with("LOG_ENABLE=") {
-            log_enable = Some(line.trim_start_matches("LOG_ENABLE=").to_string());
+    for line_result in reader.lines() {
+        let line = line_result.ok()?;
+        let trimmed_line = line.trim();
+
+        if trimmed_line.starts_with('#') || trimmed_line.is_empty() {
+            // Ignore comments and empty lines
+            continue;
+        }
+
+        if let Some((key, value)) = trimmed_line.split_once('=') {
+            match key {
+                "API_URL" => api_url = Some(value.to_string()),
+                "API_KEY" => api_key = Some(value.to_string()),
+                "REPORT_TIME" => report_time = Some(value.to_string()),
+                "MEDIA_ENABLE" => media_enable = Some(value.to_string()),
+                "LOG_ENABLE" => log_enable = Some(value.to_string()),
+                _ => {
+                    // Handle unknown keys if necessary
+                    // For now, we just ignore them
+                }
+            }
         }
     }
 
