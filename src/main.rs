7926f11ec9589e57dfaf1fb77e48d7ef2cc7b1d9
timochan <i@timochan.cn>
2023-07-28 17:38:51 +0800

    fix: cache overtime time

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
  * @Author: timochan
  * @Date: 2023-07-17 11:48:02
  * @LastEditors: timochan
- * @LastEditTime: 2023-07-27 20:36:27
+ * @LastEditTime: 2023-07-28 17:35:41
  * @FilePath: /processforlinux/src/main.rs
  */
 mod get_active_window;
@@ -13,32 +13,33 @@ mod reportprocess;
 use chrono::Utc;
 use lazy_static::lazy_static;
 use std::process::exit;
-use std::{error::Error, io::Write, sync::Mutex, time::Duration};
+use std::{error::Error, sync::Mutex, time::Duration};
 use tokio::time::sleep;
 
 type ApiVariables = (String, String, i64, bool, bool);
+
+struct Config {
+    api_url: String,
+    api_key: String,
+    watch_time: i64,
+    media_enable: bool,
+    log_enable: bool,
+}
+
 lazy_static! {
     static ref API_VARIABLES: Mutex<ApiVariables> =
         Mutex::new(get_env_file::init().unwrap_or_else(|err| {
             eprintln!("Failed to get env file: {}", err);
             exit(1);
         }));
-    static ref PREVIOUS_PROCESS_NAME: Mutex<Option<String>> = Mutex::new(None);
-    static ref PREVIOUS_MEDIA_METADATA: Mutex<Option<get_media::MediaMetadata>> = Mutex::new(None);
 }
-
-async fn run_loop(
-    api_key: &str,
-    api_url: &str,
-    watch_time: i64,
-    media_enable: bool,
-    log_enable: bool,
-) {
+async fn run_loop(config: Config) {
+    let mut last_time = Utc::now();
+    let mut previous_process_name = String::new();
+    let mut previous_media_metadata: get_media::MediaMetadata = get_media::MediaMetadata::default();
     loop {
-        std::io::stdout().flush().unwrap();
-        std::io::stderr().flush().unwrap();
-
-        let media_metadata = if media_enable {
+        let utc_now = Utc::now();
+        let media_metadata = if config.media_enable {
             get_media::get_media_metadata().unwrap_or_default()
         } else {
             get_media::MediaMetadata::default()
@@ -51,43 +52,44 @@ async fn run_loop(
                 continue;
             }
         };
-        let prev_process_name = PREVIOUS_PROCESS_NAME.lock().unwrap().clone();
-        let prev_media_metadata = PREVIOUS_MEDIA_METADATA.lock().unwrap().clone();
 
-        if prev_process_name.as_ref() != Some(&process_name)
-            || prev_media_metadata.as_ref() != Some(&media_metadata)
+        let prev_process_name = previous_process_name.clone();
+        let prev_media_metadata = previous_media_metadata.clone();
+
+        if prev_process_name != process_name
+            || prev_media_metadata != media_metadata
+            || (utc_now - last_time).num_seconds() > 60
         {
             if let Err(e) = report(
                 &process_name,
                 &media_metadata.title.clone().unwrap_or_default(),
                 &media_metadata.artist.clone().unwrap_or_default(),
-                &api_key,
-                &api_url,
-                watch_time,
-                log_enable,
+                &config,
             )
             .await
             {
                 eprintln!("Failed to report: {}", e);
             }
 
-            *PREVIOUS_PROCESS_NAME.lock().unwrap() = Some(process_name.clone());
-            *PREVIOUS_MEDIA_METADATA.lock().unwrap() = Some(media_metadata.clone());
+            previous_process_name = process_name;
+            previous_media_metadata = media_metadata;
+            last_time = utc_now;
         } else {
-            if log_enable {
+            if config.log_enable {
                 let utc_now = Utc::now();
                 let next_watch_time = utc_now
-                    .checked_add_signed(chrono::Duration::seconds(watch_time))
+                    .checked_add_signed(chrono::Duration::seconds(config.watch_time))
                     .unwrap()
                     .format("%Y-%m-%d %H:%M:%S");
-
+                let utc_now = utc_now.format("%Y-%m-%d %H:%M:%S");
                 println!("--------------------------------------------------");
+                println!("This Watch Time : {}", utc_now);
                 println!("No change in process or media metadata");
                 println!("Next Watch Time : {}", next_watch_time);
                 println!("--------------------------------------------------");
             }
         }
-        let sleep_interval_secs = watch_time.to_string().parse::<u64>().unwrap_or(60);
+        let sleep_interval_secs = config.watch_time.to_string().parse::<u64>().unwrap_or(5);
         sleep(Duration::from_secs(sleep_interval_secs)).await;
     }
 }
@@ -96,19 +98,16 @@ async fn report(
     process_name: &str,
     media_title: &str,
     media_artist: &str,
-    api_key: &str,
-    api_url: &str,
-    watch_time: i64,
-    log_enable: bool,
+    config: &Config,
 ) -> Result<(), Box<dyn Error>> {
     reportprocess::process_report(
         process_name,
         media_title,
         media_artist,
-        api_key,
-        api_url,
-        watch_time,
-        log_enable,
+        &config.api_key,
+        &config.api_url,
+        config.watch_time,
+        config.log_enable,
     )
     .await?;
     Ok(())
@@ -120,5 +119,12 @@ async fn main() {
         let api_vars = API_VARIABLES.lock().unwrap();
         api_vars.clone()
     };
-    run_loop(&api_key, &api_url, watch_time, media_enable, log_enable).await;
+    let config = Config {
+        api_url: api_url,
+        api_key: api_key,
+        watch_time: watch_time,
+        media_enable: media_enable,
+        log_enable: log_enable,
+    };
+    run_loop(config).await;
 }
